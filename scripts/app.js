6196de0ed2f215dd2127b1babbde735a45a3b5f2
Luma <luma@marinellaluft.com>
2020-09-25 14:15:40 +0200

    terminata parte responsive

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+   const header = document.getElementById("header")
+
+   window.onscroll = function (event) {
+      if (window.scrollY > 1) {
+         header.classList.add("header__scrolling")
+      } else {
+         header.classList.remove("header__scrolling")
+      }
+   }
+} 
